95d3b278d63294da4410fdee0ca19012e7bd5edc
ignatiustan123 <tanignatiustan@gmail.com>
2023-09-10 02:21:23 +0900

    Updated restaurants_airflow.py
    Included pipeline from local file to Google Cloud Storage and to a table in BigQuery for storage.

--- restaurants_airflow.py
+++ restaurants_airflow.py
@@ -4,6 +4,8 @@ from urllib.request import urlopen
 import os
 from datetime import datetime
 from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow import DAG
 
 def task_1_processing():
@@ -86,5 +88,33 @@ with DAG(
          task_id = 'process_data',
          python_callable=task_1_processing
     )
+    
+    to_gcs = LocalFilesystemToGCSOperator(
+        task_id = "to_gcs",
+        gcp_conn_id = 'gcp_dataengineering',
+        bucket = 'engineerproj',
+        src = '/tmp/restaurants.csv',
+        dst = 'restaurants'   
+    )
 
-task_process_data
+    to_bq = GCSToBigQueryOperator(
+        task_id= 'to_bq',
+        gcp_conn_id = 'gcp_dataengineering',
+        bucket = 'engineerproj',
+        source_objects=['restaurants'],
+        destination_project_dataset_table= 'data-engineering-398512.result_tables.restaurants',
+        schema_fields=[
+        {'name':'Restaurant Id','type':'STRING','mode':'NULLABLE'},
+        {'name':'Restaurant Name','type':'STRING','mode':'NULLABLE'},
+        {'name':'Country','type':'STRING','mode':'NULLABLE'},
+        {'name':'City','type':'STRING','mode':'NULLABLE'},
+        {'name':'User Rating Votes','type':'INTEGER','mode':'NULLABLE'},
+        {'name':'User Aggregate Rating','type':'FLOAT','mode':'NULLABLE'},
+        {'name':'Cuisines','type':'STRING','mode':'NULLABLE'},
+        ],
+        create_disposition = 'CREATE_IF_NEEDED',    
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1
+    )
+    
+task_process_data >> to_gcs >> to_bq
